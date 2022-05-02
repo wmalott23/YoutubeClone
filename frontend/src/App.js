155d3622d9eb1db040f41baaa257115c497fe6ab
WTDula <wtdula@gmail.com>
2022-05-02 14:02:39 -0500

    tested SearchPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 // General Imports
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import "./files.env"
 
 // Pages Imports
 import HomePage from "./pages/HomePage/HomePage";
@@ -11,9 +10,11 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import SearchPage from "./components/SearchPage/SearchPage";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
+
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
             </PrivateRoute>
           }
         />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
--- frontend/src/components/SearchPage/SearchPage.jsx
+++ frontend/src/components/SearchPage/SearchPage.jsx
@@ -1,10 +1,30 @@
+import axios from "axios";
+import React, { useState } from "react";
 
-
+const API_KEY = process.env.REACT_APP_API_KEY
 const SearchPage = (props) => {
+
+    const [videos, setVideos] = useState({})
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        console.log(searchTerm)
+        try{
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=${API_KEY}`)
+            setVideos(response.data)
+        }
+        catch(e){
+            console.log(e.message)
+        }
+        console.log(videos)
+    }
+
     return ( 
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Search Terms" value={searchTerm} onChange={(event) => {setSearchTerm(event.target.value)}}/>
+            <button type="submit"></button>
+        </form>
      );
 }
  
